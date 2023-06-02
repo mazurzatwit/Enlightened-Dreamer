704824ced60ff83008513a253ce9861c34ebcb6c
Singhs17 <singhs17@wit.edu>
2023-06-02 11:35:20 -0400

    Initial Setup (test commit)

---
+++ main.py
@@ -0,0 +1,9 @@
+## Enlightened Dreamer will help our users delve deeper into their dreams and 
+## sleep patterns. By tracking sleep patterns, users are able to analyze what 
+## their dreams mean and how they might relate to their current state of mind. 
+## The project consists of a web application supported by a backend that controls
+## all user interaction. This user interaction includes recording dreams, a web 
+## connection, and collects data on the user’s sleep patterns to be stored in a 
+## secondary storage. The data will be displayed by a GUI via the use of the backend. 
+
+print( "Let's Code a Project Ladies :))) ")
