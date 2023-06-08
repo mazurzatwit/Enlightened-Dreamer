8b70948180935ee6c1e7fdbbaf09d54e649cc754
bernashenatwit <bernashen@wit.edu>
2023-06-08 14:50:58 -0400

    Initial Database Connection

--- main.py
+++ main.py
@@ -6,4 +6,30 @@
 ## connection, and collects data on the user’s sleep patterns to be stored in a 
 ## secondary storage. The data will be displayed by a GUI via the use of the backend. 
 
+##  python3 -m pip install mysql-connector-python
+import mysql.connector
+
 print( "Let's Code a Project Ladies :))) ")
+
+## Initial Connection
+connection = mysql.connector.connect(host='localhost',
+                                         database='enlightened dreamer',
+                                         user='root')
+
+## Checks SQL connection
+if connection.is_connected():
+    db_Info = connection.get_server_info()
+    print("Connected to MySQL Server version ", db_Info)
+    cursor = connection.cursor()
+    cursor.execute("select database();")
+    record = cursor.fetchone()
+    print("You're connected to database: ", record[0])
+
+    user_permissions = "SELECT * FROM USERS WHERE Username = 'Elle' AND aes_decrypt(Password, 'PASS') = 'L0nd0nGirl';"
+    cursor = connection.cursor()
+    cursor.execute(user_permissions)
+    # get all records
+    user_data = cursor.fetchall()
+
+    for user in user_data:
+        print("Welcome", user[3])
