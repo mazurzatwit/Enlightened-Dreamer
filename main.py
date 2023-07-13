766953b017887f83f01a31a0a1847115b104ab55
Singhs17 <singhs17@wit.edu>
2023-07-13 14:47:58 -0400

    tips page up and running! (maybe not the back_btn)

--- main.py
+++ main.py
@@ -47,6 +47,7 @@ class main_window(QMainWindow):
           self.login_page = self.findChild(QWidget, "login_page")
           self.sign_up_page = self.findChild(QWidget, "sign_up_page")
           self.dashboard = self.findChild(QWidget, "dashboard_page")
+          self.tips = self.findChild(QWidget, "tips_page")
 
           self.ui_stack.setCurrentWidget(self.home)
           
@@ -58,7 +59,7 @@ class main_window(QMainWindow):
           
           #login page button
           self.login_btn = self.findChild(QPushButton, "login_btn")
-          self.back_button = self.findChild(QPushButton, "back_button")
+          self.back_button = self.findChild(QPushButton, "back_btn1")
           self.username_text = self.findChild(QLineEdit, "username_textbox")
           self.password_text = self.findChild(QLineEdit, "password_textbox")
           self.login_btn.clicked.connect(self.login_save_click)
@@ -66,7 +67,7 @@ class main_window(QMainWindow):
 
           #sign up page button
           self.submit_button = self.findChild(QPushButton, "submit_button")
-          self.back_btn = self.findChild(QPushButton, "back_btn")
+          self.back_btn = self.findChild(QPushButton, "back_btn3")
           self.name_text = self.findChild(QLineEdit, "name_textbox")
           self.email_text = self.findChild(QLineEdit, "email_textbox")
           self.uname_text = self.findChild(QLineEdit, "uname_textbox")
@@ -78,8 +79,6 @@ class main_window(QMainWindow):
           self.dream_info_btn = self.findChild(QPushButton, "dream_info_button")
           self.dashboard_view1 = dashboard.dashboard_view(0,0,0,0,0)
           self.dream_info_btn.clicked.connect(self.dashboard_view1.dream_info_popup)
-
-          #dashboard buttons
           self.sleep_time = self.findChild(QLineEdit, "fell_asleep_time")
           self.wake_time = self.findChild(QLineEdit, "wake_up_time")
           self.dream = self.findChild(QTextEdit, "dream_text_edit")
@@ -87,6 +86,11 @@ class main_window(QMainWindow):
           self.dashboard_save_btn = self.findChild(QPushButton, "save_button")
           self.dashboard_save_btn.clicked.connect(self.dashboard_save)
 
+          #tips buttons
+          self.tips_btn = self.findChild(QPushButton, "sleep_resources_btn")
+          self.tips_btn.clicked.connect(self.show_tips_page)
+          self.back_tips = self.findChild(QPushButton, "back_btn2")
+          self.back_btn.clicked.connect(self.show_dashboard)
 
          
      def login_save_click(self):
@@ -129,7 +133,6 @@ class main_window(QMainWindow):
           self.dash_save.save_new_dream()
 
 
-
      def show_sign_up_page(self):
           self.ui_stack.setCurrentWidget(self.sign_up_page)
 
@@ -144,6 +147,12 @@ class main_window(QMainWindow):
           self.pass_text.clear()
           self.username_text.clear()
           self.password_text.clear()
+
+     def show_dashboard(self):
+          self.ui_stack.setCurrentWidget(self.dashboard)
+
+     def show_tips_page(self):
+          self.ui_stack.setCurrentWidget(self.tips)
 
 
 
--- tips.py
+++ tips.py
@@ -3,4 +3,3 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5 import uic
 import mysql.connector
 
-print( "This can be the hollistic tips page :))) ")
